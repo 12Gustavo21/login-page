beb7ab2cc5e70a4b5c8b91386f587bba87ab1733
12Gustavo21 <gustavo927318@gmail.com>
2025-01-20 16:40:17 -0300

    style: change querySelector form class name

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ function validateForm() {
   }
 }
 
-const form = document.querySelector(".form");
+const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
